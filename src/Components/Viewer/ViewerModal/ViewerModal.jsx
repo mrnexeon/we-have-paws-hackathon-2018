4deedd4ccd8bd4f5dd1f731c6ba28b33828917e7
mrnexeon <the.nexeon@gmail.com>
2018-05-01 14:35:27 +0300

    renamed property, removed never used files

--- src/Components/Viewer/ViewerModal/ViewerModal.jsx
+++ src/Components/Viewer/ViewerModal/ViewerModal.jsx
@@ -4,7 +4,7 @@ import './ViewerModal.css'
 class ViewerModal extends Component {
     render() {
         return (
-            <div className={this.props.open ? 'modal-box' : 'hidden'}>
+            <div className={this.props.isOpen ? 'modal-box' : 'hidden'}>
                 {this.props.children}
             </div>
         );
